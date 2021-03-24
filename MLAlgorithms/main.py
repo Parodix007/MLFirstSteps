89457c89cc96d71012fccfcef2a74c983330dd26
Parodix007 <sebastian.siarczynski@gmail.com>
2021-03-24 14:26:28 +0100

    Enter to Data Science

--- MLAlgorithms/main.py
+++ MLAlgorithms/main.py
@@ -1,53 +1,18 @@
-# Supervised Machine Learning Algorithms
-from sklearn.datasets import load_iris, load_breast_cancer
-from sklearn.model_selection import train_test_split
+from IPython.core.pylabtools import figsize
+from sklearn.datasets import load_iris
+import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plo
-from sklearn.neighbors import KNeighborsClassifier
-# * *  Simple iris species machine learning
-dataset = load_iris()
+import matplotlib.pyplot as plt
+iris = load_iris()
+colors = list()
 
+colors_opt = {0: "red", 1: "green", 2: "blue"}
 
-X_train, X_test, y_train, y_test = train_test_split(
-        dataset['data'], dataset['target'], random_state=0)
+for n in np.nditer(iris.target):
+    colors.append(colors_opt[int(n)])
 
-knn = KNeighborsClassifier(n_neighbors=1)
+data_frame = pd.DataFrame(iris.data, columns=iris.feature_names)
 
-knn.fit(X_train, y_train)
+sc = pd.plotting.scatter_matrix(data_frame, alpha=0.3, figsize=(10, 10), diagonal="hist", color=colors, marker="o", grid=True)
 
-X_new = np.array([[5, 2.9, 1, 0.2]])
-print(X_new.shape)
-
-prediction = knn.predict(X_new)
-print(prediction)
-print(f'prediction name {dataset["target_names"][prediction]}')
-
-y_pred = knn.predict(X_test)
-
-print(y_pred)
-
-print(f'test score: {np.mean(y_pred == y_test)}')
-
-print("--------------")
-# * KNeighbors from sklearn
-
-bc = load_breast_cancer()
-
-bcx_train, bcx_test, bcy_train, bcy_test = train_test_split(bc.data, bc.target, random_state=0)
-
-train_acc = []
-test_acc = []
-number_of_n = range(1, 101)
-for n in number_of_n:
-        model = KNeighborsClassifier(n_neighbors=n)
-        model.fit(bcx_train, bcy_train)
-        train_acc.append(model.score(bcx_train, bcy_train))
-        test_acc.append(model.score(bcx_test, bcy_test))
-
-# Show acc of train set and test set
-plo.plot(number_of_n, train_acc, label="train acc")
-plo.plot(number_of_n, test_acc, label="test acc")
-plo.ylabel("Accuracy")
-plo.xlabel("n_neighbors")
-plo.legend()
-plo.show()
+plt.show()
